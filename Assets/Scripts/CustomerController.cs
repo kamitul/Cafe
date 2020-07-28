4d3fba3f1addb24578ac392e6df83cd92335313b
Kamil Tulczyjew <kamcio819@gmail.com>
2020-07-28 18:34:54 +0200

    Added gif player

---
+++ Assets/Scripts/CustomerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CustomerController : MonoBehaviour
+{
+    [SerializeField]
+    private List<ITickable> tickables;
+
+    private void Awake()
+    {
+        tickables = GetComponents<ITickable>().ToList();
+    }
+}
---
+++ Assets/Scripts/GifController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class GifController : MonoBehaviour, ITickable
+{
+    [SerializeField]
+    private List<Sprite> sprites;
+
+    public int TickTime = 100;
+
+    private SpriteRenderer spriteRenderer;
+    private int index = 0;
+    private CancellationTokenSource cancelToken;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        cancelToken = new CancellationTokenSource();
+    }
+
+    private async void Start()
+    {
+        try
+        {
+            await AnimateGif();
+        }
+        catch (TaskCanceledException)
+        {
+            return;
+        }
+    }
+
+    private async Task AnimateGif()
+    {
+        while (true)
+        {
+            if (cancelToken.IsCancellationRequested)
+                throw new TaskCanceledException();
+
+            spriteRenderer.sprite = sprites[++index%sprites.Count];
+            await Task.Delay(TickTime);
+        }
+    }
+
+    public void Tick()   
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        cancelToken.Cancel();
+    }
+}
