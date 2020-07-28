68d656c8e048eb048a2cc01d0df00edd55ff87fb
Kamil Tulczyjew <kamcio819@gmail.com>
2020-07-28 13:09:44 +0200

    Finished generator

--- Assets/Scripts/BasicCafe.cs
+++ Assets/Scripts/BasicCafe.cs
@@ -33,16 +33,16 @@ public class BasicCafe : IBuilder
             tiles[i] = new BoxTile[height];
         }
 
-        CreateFloor(tiles);
-        CreateDimensions(tiles);
-        CreateEnviroment(engine, rand, tiles);
-        CreateWindows(tiles);
-        CreateDoor(engine, tiles);
+        CreateFloor();
+        CreateDimensions();
+        CreateEnviroment(engine, rand);
+        CreateWindows();
+        CreateDoor(engine);
 
         mapMatrix.SetTiles(tiles);
     }
 
-    private void CreateFloor(BoxTile[][] tiles)
+    private void CreateFloor()
     {
         for (int i = 1; i < width - 1; ++i)
         {
@@ -53,14 +53,14 @@ public class BasicCafe : IBuilder
         }
     }
 
-    private int CreateDoor(System.Random engine, BoxTile[][] tiles)
+    private int CreateDoor(System.Random engine)
     {
         int rand = engine.Next(2, width - 2);
         tiles[rand][0] = new BoxTile(new Vector3Int(rand, 0, 0), 5);
         return rand;
     }
 
-    private void CreateWindows(BoxTile[][] tiles)
+    private void CreateWindows()
     {
         for (int i = 2; i < width; ++i)
         {
@@ -72,7 +72,7 @@ public class BasicCafe : IBuilder
         }
     }
 
-    private int CreateEnviroment(System.Random engine, int rand, BoxTile[][] tiles)
+    private int CreateEnviroment(System.Random engine, int rand)
     {
         for (int i = 0; i < 2; i++)
         {
@@ -83,7 +83,7 @@ public class BasicCafe : IBuilder
         return rand;
     }
 
-    private void CreateDimensions(BoxTile[][] tiles)
+    private void CreateDimensions()
     {
         for (int i = 0; i < width; ++i)
         {
@@ -100,6 +100,32 @@ public class BasicCafe : IBuilder
 
     public void BuildMisc()
     {
+        CreateTables();
+        CreateBar();
+
+        mapMatrix.SetTiles(tiles);
+
+    }
+
+    private void CreateBar()
+    {
+        Vector3Int barStart = new Vector3Int(width - 5, 1, 0);
+
+        for (; barStart.y < 7; barStart.y++)
+        {
+            tiles[barStart.x][barStart.y].AddId(9);
+        }
+
+        Vector3Int barEnd = new Vector3Int(barStart.x + 1, barStart.y - 1, 0);
+
+        for (; barEnd.x < width - 1; barEnd.x++)
+        {
+            tiles[barEnd.x][barEnd.y].AddId(8);
+        }
+    }
+
+    private void CreateTables()
+    {
         Vector3Int leftTop = new Vector3Int(1, height - 2, 0);
         Vector3Int rightDown = new Vector3Int(width - 8, 2, 0);
         Vector3Int rightTop = new Vector3Int(rightDown.x, leftTop.y, 0);
@@ -107,19 +133,33 @@ public class BasicCafe : IBuilder
 
         Vector3Int center = new Vector3Int(rightDown.x + leftTop.x, leftTop.y + rightDown.y, leftTop.z + rightDown.z) / 2;
 
-        Tuple<int, int> indexes = mapMatrix.GetIndexOf((center + leftTop) / 2);
-        tiles[indexes.Item1][indexes.Item2].AddId(7);
+        CreateTable((center + leftTop) / 2);
+        CreateTable((center + leftDown) / 2);
+        CreateTable((center + rightTop) / 2);
+        CreateTable((center + rightDown) / 2);
+    }
 
-        indexes = mapMatrix.GetIndexOf((center + leftDown) / 2);
+    private void CreateTable(Vector3Int offset)
+    {
+        Tuple<int, int> indexes = mapMatrix.GetIndexOf(offset);
         tiles[indexes.Item1][indexes.Item2].AddId(7);
+        CreateStools(offset);
+    }
 
-        indexes = mapMatrix.GetIndexOf((center + rightTop) / 2);
-        tiles[indexes.Item1][indexes.Item2].AddId(7);
+    private void CreateStools(Vector3Int vector3Int)
+    {
+        Tuple<int, int> indexes = mapMatrix.GetIndexOf(vector3Int + new Vector3Int(1,1, 0));
+        tiles[indexes.Item1][indexes.Item2].AddId(10);
 
-        indexes = mapMatrix.GetIndexOf((center + rightDown) / 2);
-        tiles[indexes.Item1][indexes.Item2].AddId(7);
+        indexes = mapMatrix.GetIndexOf(vector3Int + new Vector3Int(-1, 1, 0));
+        tiles[indexes.Item1][indexes.Item2].AddId(10);
 
-        mapMatrix.SetTiles(tiles);
+        indexes = mapMatrix.GetIndexOf(vector3Int + new Vector3Int(-1, -1, 0));
+        tiles[indexes.Item1][indexes.Item2].AddId(10);
+
+        indexes = mapMatrix.GetIndexOf(vector3Int + new Vector3Int(1, -1, 0));
+        tiles[indexes.Item1][indexes.Item2].AddId(10);
+
     }
 
     public MapMatrix GetProduct()
