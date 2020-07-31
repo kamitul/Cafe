75e73265fc1ca568c6741ae6ea00b87a182e5642
Kamil Tulczyjew <kamcio819@gmail.com>
2020-07-31 18:48:12 +0200

    no message

--- Assets/Scripts/OrderIndicatorGUI.cs
+++ Assets/Scripts/OrderIndicatorGUI.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class OrderIndicatorGUI : MonoBehaviour
+public class OrderIndicatorGUI : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField]
     private CustomerOrderController customerOrder;
 
     public void OnMouseDown()
+    {
+        customerOrder.MakeOrder();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
     {
         customerOrder.MakeOrder();
     }
